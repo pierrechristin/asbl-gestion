a4918276aa8c465bd300fe47239a51f77d45d24f
Pierre Christin <p.christin@kiabi.com>
2018-03-01 12:57:02 +0100

    chore: reformatage du fichier d'enregistrement des bénéficiaires

--- beneficiaire/enregistrer.php
+++ beneficiaire/enregistrer.php
@@ -9,7 +9,22 @@ $module=$_POST['module'];
 
 if ($module==1 and $_POST['reference']<>0 and isset($_SESSION['idUtilisateur']))
 { 
-$sql="update beneficiaires set nom=\"".$_POST['nom']."\",prenom=\"".$_POST['prenom']."\",tel=\"".$_POST['tel']."\",adresse=\"" .$_POST['adresse']."\",nbracharge=\"".$_POST['nbracharge']."\",solde_colis=\"".$_POST['solde_colis']."\",solde_magasin=\"".$_POST['solde_magasin']."\",sem1=\"".isset($_POST['sem1'])."\",sem2=\"".isset($_POST['sem2'])."\",sem3=\"".isset($_POST['sem3'])."\",sem4=\"".isset($_POST['sem4'])."\",jourpassage=\"".$_POST['jourPassage']."\",heurepassage=\"".date('H:i:s',mktime($_POST['heurePassage'],$_POST['minutePassage'],0))."\",id_utilisateur_modification=".$_SESSION['idUtilisateur']." where ref=".$_POST['reference'].";";
+$sql='update beneficiaires'.' '
+		.'set nom="'.$_POST['nom'].'",'
+			.'prenom="'.$_POST['prenom'].'",'
+			.'tel="'.$_POST['tel'].'",'
+			.'adresse="'.$_POST['adresse'].'",'
+			.'nbracharge="'.$_POST['nbracharge'].'",'
+			.'solde_colis="'.$_POST['solde_colis'].'",'
+			.'solde_magasin="'.$_POST['solde_magasin'].'",'
+			.'sem1="'.isset($_POST['sem1']).'",'
+			.'sem2="'.isset($_POST['sem2']).'",'
+			.'sem3="'.isset($_POST['sem3']).'",'
+			.'sem4="'.isset($_POST['sem4']).'",'
+			.'jourpassage="'.$_POST['jourPassage'].'",'
+			.'heurepassage="'.date('H:i:s',mktime($_POST['heurePassage'],$_POST['minutePassage'],0)).'",'
+			.'id_utilisateur_modification='.$_SESSION['idUtilisateur'].' '
+		.'where ref='.$_POST['reference'].';';
 $base->exec($sql);
 
 }
@@ -18,8 +33,36 @@ $base->exec($sql);
 
 if ($module==2 and $_POST['reference']==0 and isset($_SESSION['idUtilisateur']))
 { 
-$sql="insert into beneficiaires (nom,prenom,tel,adresse,nbracharge,solde_colis,solde_magasin,sem1,sem2,sem3,sem4,jourpassage,heurepassage,id_utilisateur_creation)";
-$sql=$sql." values (\"".$_POST['nom']."\",\"".$_POST['prenom']."\",\"".$_POST['tel']."\",\"".$_POST['adresse']."\",\"".$_POST['nbracharge']."\",\"".$_POST['solde_colis']."\",\"".$_POST['solde_magasin']."\",\"".isset($_POST['sem1'])."\",\"".isset($_POST['sem2'])."\",\"".isset($_POST['sem3'])."\",\"".isset($_POST['sem4'])."\",\"".$_POST['jourPassage']."\",\"".date('H:i:s',mktime($_POST['heurePassage'],$_POST['minutePassage'],0))."\",".$_SESSION['idUtilisateur'].")";
+$sql='insert into beneficiaires ('
+			.'nom,'
+			.'prenom,'
+			.'tel,'
+			.'adresse,'
+			.'nbracharge,'
+			.'solde_colis,'
+			.'solde_magasin,'
+			.'sem1,'
+			.'sem2,'
+			.'sem3,'
+			.'sem4,'
+			.'jourpassage,'
+			.'heurepassage,'
+			.'id_utilisateur_creation'
+		.') values ('
+		.'"'.$_POST['nom'].'",'
+		.'"'.$_POST['prenom'].'",'
+		.'"'.$_POST['tel'].'",'
+		.'"'.$_POST['adresse'].'",'
+		.'"'.$_POST['nbracharge'].'",'
+		.'"'.$_POST['solde_colis'].'",'
+		.'"'.$_POST['solde_magasin'].'",'
+		.'"'.isset($_POST['sem1']).'",'
+		.'"'.isset($_POST['sem2']).'",'
+		.'"'.isset($_POST['sem3']).'",'
+		.'"'.isset($_POST['sem4']).'",'
+		.'"'.$_POST['jourPassage'].'",'
+		.'"'.date('H:i:s',mktime($_POST['heurePassage'],$_POST['minutePassage'],0)).'",'
+		.'"'.$_SESSION['idUtilisateur'].')';
 $base->exec($sql);
 }
 
@@ -27,11 +70,16 @@ $base->exec($sql);
 
 if ($module==3 and $_POST['reference']<>0 and isset($_SESSION['idUtilisateur']))
 { 
-$sql="delete from beneficiaires where ref=".$_POST['reference'];
+$sql='delete from beneficiaires'.' '
+		.'where ref='.$_POST['reference'];
 $base->exec($sql);
 	
 // On indique quel utilisateur a supprimé ce beneficiare
-$sql2="update beneficiaires_historique set id_utilisateur_histo=".$_SESSION['idUtilisateur']." where ref=".$_POST['reference']." ORDER BY date_histo DESC LIMIT 1";
+$sql2='update beneficiaires_historique'.' '
+		.'set id_utilisateur_histo='.$_SESSION['idUtilisateur'].' '
+		.'where ref='.$_POST['reference'].' '
+		.'ORDER BY date_histo DESC'.' '
+		.'LIMIT 1';
 $base->exec($sql2);
 }
 
